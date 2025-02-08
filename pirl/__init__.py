de85fae75f41dd53ced731075d24adcafddca36b
PIRLcorp <pioncorpp@gmail.com>
2025-02-09 00:16:29 +0200

    Update __init__.py

--- pirl/__init__.py
+++ pirl/__init__.py
@@ -1,1 +1,13 @@
+# pirl/__init__.py
 
+from .ai_tools import IngredientDiscovery
+from .fnb import FnbAnalysis
+from .life_sciences import LifeSciencesResearch
+from .nutraceuticals import NutraceuticalsDevelopment
+
+__all__ = [
+    "IngredientDiscovery",
+    "FnbAnalysis",
+    "LifeSciencesResearch",
+    "NutraceuticalsDevelopment",
+]
